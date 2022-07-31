c256bd85f6a646227dee540633008315efc991bb
kavitha2712 <81770655+kavitha2712@users.noreply.github.com>
2022-07-31 13:37:37 -0700

    Update chart4.js

--- scripts/chart4.js
+++ scripts/chart4.js
@@ -1,18 +1,18 @@
-var finalDataChart3 = [];
+var finalDataChart4 = [];
 // initialise layout variables
-const marginChart3 = {top: 50, right: 20, bottom: 50, left: 60};
-const widthChart3 = 600;
-const heightChart3 = 400;
+const marginChart4 = {top: 50, right: 20, bottom: 50, left: 60};
+const widthChart4 = 600;
+const heightChart4 = 400;
 
 // initialise charts
-const svgChart3 = d3.select('#svg3')
-    .attr('width', widthChart3 + marginChart3.left + marginChart3.right)
-    .attr('height', heightChart3 + marginChart3.top + marginChart3.bottom)
+const svgChart4 = d3.select('#svg4')
+    .attr('width', widthChart4 + marginChart4.left + marginChart4.right)
+    .attr('height', heightChart4 + marginChart4.top + marginChart4.bottom)
     .append('g')
-    .attr('transform', 'translate(' + marginChart3.left + ',' + marginChart3.top + ')')
-    .attr('id', 'svg-3-parent-g');
+    .attr('transform', 'translate(' + marginChart4.left + ',' + marginChart4.top + ')')
+    .attr('id', 'svg-4-parent-g');
 
-charts.chart3 = function() {
+charts.chart4 = function() {
     getDataAndDraw();
 }
 
@@ -30,14 +30,14 @@ function getDataAndDraw() {
 //             d.year = d.date.getFullYear();
 //         });
 
-        paramsChart3.forEach(function(param) {
+        paramsChart4.forEach(function(param) {
             if (!d3.select(param.id).property('checked')) {
                 data = data.filter(d => d["State Code"] != param.statecd);
             }
         });
 
         const dataGroupedByRegion = Array.from(d3.group(data, d => d["State Code"]));
-        finalDataChart3 = dataGroupedByRegion.map(
+        finalDataChart4 = dataGroupedByRegion.map(
             function (item) {
                 var sumDeaths = 0;
                 item[1].forEach(d => sumDeaths += d["Total_Deaths_2019"]);
@@ -50,23 +50,23 @@ function getDataAndDraw() {
            ).sort()
 //         ).sort((a, b) => (a.genre > b.genre) ? 1 : -1);
 
-        drawChart3(finalDataChart3);
+        drawChart4(finalDataChart4);
     });
 }
 
-function drawChart3(data) {
-    d3.select('#svg-3-parent-g').selectAll('*').remove();
-    svgChart3.selectAll('rect').remove();
+function drawChart4(data) {
+    d3.select('#svg-4-parent-g').selectAll('*').remove();
+    svgChart4.selectAll('rect').remove();
 
     // X axis
     const x = d3.scaleBand()
-        .range([0, widthChart3])
+        .range([0, widthChart4])
         .domain(data.map(function (d) {
             return d.state;
         }))
         .padding(0.2);
-    svgChart3.append("g")
-        .attr("transform", "translate(0," + heightChart3 + ")")
+    svgChart4.append("g")
+        .attr("transform", "translate(0," + heightChart4 + ")")
         .call(d3.axisBottom(x))
         .selectAll("text")
         .attr("transform", "translate(-10,0)rotate(-45)")
@@ -74,24 +74,24 @@ function drawChart3(data) {
 
     // Add Y axis
     const y = d3.scaleLinear()
-        .domain([0, 5300])
-        .range([heightChart3, 0]);
-    svgChart3.append("g")
+        .domain([0, 5400])
+        .range([heightChart4, 0]);
+    svgChart4.append("g")
         .call(d3.axisLeft(y));
 
     // Bars
-    svgChart3.selectAll("mybar")
+    svgChart4.selectAll("mybar")
         .data(data)
         .enter()
         .append("rect")
         .attr("x", function(d) { return x(d.state); })
         .attr("y", function(d) { return y(d.numDeaths); })
         .attr("width", x.bandwidth())
-        .attr("height", function(d) { return heightChart3 - y(d.numDeaths); })
+        .attr("height", function(d) { return heightChart4 - y(d.numDeaths); })
         .attr("fill", "#965086");
 }
 
-const paramsChart3 = [
+const paramsChart4 = [
     {
         id: "#checkbox-CA",
         statecd: "CA"
@@ -113,6 +113,6 @@ const paramsChart3 = [
         statecd: "OH"
     },
 ];
-function updateChart3Data() {
+function updateChart4Data() {
     getDataAndDraw();
 }
